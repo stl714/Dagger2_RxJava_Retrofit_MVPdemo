4af8cc4447b3f67bf02029e3300be5ee5bcbd74b
taolin <taolin.sun@pantum.local>
2020-01-03 14:54:33 +0800

    initial commit

---
+++ app/src/main/java/com/example/dagger2_rxjava_retrofit_mvpdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dagger2_rxjava_retrofit_mvpdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
